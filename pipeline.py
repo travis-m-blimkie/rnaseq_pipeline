a1bbafea5cd2cae9833ec1ec2b0f34bf024639f4
Travis Blimkie <travis.m.blimkie@gmail.com>
2026-06-23 12:18:23 -0700

    Used Posit Assistant to refactor code to adhere to PEP8 guidelines

--- pipeline.py
+++ pipeline.py
@@ -1,9 +1,10 @@
+import argparse
 import os
 import subprocess as sp
-import pandas as pd
 from glob import glob
 from re import search
-import argparse
+
+import pandas as pd
 
 parser = argparse.ArgumentParser(description="Pipeline parameters")
 
@@ -55,7 +56,7 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-## Define input parameters
+# Define input parameters
 sample_sheet = args.sample_sheet
 genome_dir = args.genome_dir
 threads = args.threads
@@ -64,21 +65,21 @@ gtf = args.gtf
 do_cram = args.do_cram
 fasta = args.fasta
 
-
 df = pd.read_csv(sample_sheet)
 
 fastq_files = df[["fastq1", "fastq2"]].values.flatten().tolist()
 fastq_string = " ".join(str(f) for f in fastq_files)
 
-
-## FastQC
+# FastQC
 def run_fastqc(all_fastq_files, threads):
     os.makedirs(os.path.dirname("FastQC/"), exist_ok=True)
-    fastqc_command = "fastqc -q -t " + str(threads) + " -o FastQC/ " + all_fastq_files
+    fastqc_command = (
+        f"fastqc -q -t {threads} -o FastQC/ {all_fastq_files}"
+    )
     sp.run(fastqc_command, shell=True, check=True)
 
 
-## STAR
+# STAR
 def run_star(df, genome_dir, threads):
     os.makedirs(os.path.dirname("STAR/"), exist_ok=True)
 
@@ -105,13 +106,20 @@ def run_star(df, genome_dir, threads):
         sp.run(star_cmd, shell=True, check=True)
 
     bam_files = glob("STAR/*.bam")
-    for b in bam_files:
-        os.rename(b, b.replace("_Aligned.sortedByCoord.out", ""))
+    for bam_file in bam_files:
+        os.rename(
+            bam_file,
+            bam_file.replace("_Aligned.sortedByCoord.out", ""),
+        )
 
-    sp.run(f"STAR --genomeDir {genome_dir} --genomeLoad Remove", shell=True, check=True)
+    sp.run(
+        f"STAR --genomeDir {genome_dir} --genomeLoad Remove",
+        shell=True,
+        check=True,
+    )
 
 
-## HTSeq
+# HTSeq
 def run_htseq(df, strand, gtf_file):
     os.makedirs(os.path.dirname("HTSeq/"), exist_ok=True)
 
@@ -126,18 +134,18 @@ def run_htseq(df, strand, gtf_file):
             f"-r pos "
             f"-c HTSeq/{name}.count "
             f"STAR/{name}.bam "
-            f"{gtf_file} "
+            f"{gtf_file}"
         )
         sp.run(htseq_cmd, shell=True, check=True)
 
 
-## MultiQC
+# MultiQC
 def run_multiqc():
     multiqc_cmd = "multiqc -f -o MultiQC FastQC STAR HTSeq"
     sp.run(multiqc_cmd, shell=True, check=True)
 
 
-## Samtools/CRAM
+# Samtools/CRAM
 def run_samtools(df, threads, fasta):
     for _, row in df.iterrows():
         name = row["name"]
@@ -153,37 +161,72 @@ def run_samtools(df, threads, fasta):
         sp.run(cram_cmd, shell=True, check=True)
 
 
-## Version information
+# Version information
 def run_versions(run_cram):
-    version_fastqc = sp.check_output("fastqc --version", shell=True).decode("utf-8").replace("\n", "").replace("FastQC v", "")
+    version_fastqc = (
+        sp.check_output("fastqc --version", shell=True)
+        .decode("utf-8")
+        .replace("\n", "")
+        .replace("FastQC v", "")
+    )
 
-    version_star = sp.check_output("STAR --version", shell=True).decode("utf-8").replace("\n", "")
+    version_star = (
+        sp.check_output("STAR --version", shell=True)
+        .decode("utf-8")
+        .replace("\n", "")
+    )
 
-    version_htseq_raw = sp.check_output("htseq-count --help | grep version", shell=True, stderr=sp.STDOUT).decode("utf-8")
-    version_htseq = search("[0-9]\\.[0-9]{1,2}\\.[0-9]{1,2}", version_htseq_raw).group(0)
+    version_htseq_raw = sp.check_output(
+        "htseq-count --help | grep version",
+        shell=True,
+        stderr=sp.STDOUT,
+    ).decode("utf-8")
+    version_htseq = search(
+        "[0-9]\\.[0-9]{1,2}\\.[0-9]{1,2}",
+        version_htseq_raw,
+    ).group(0)
 
-    version_multiqc_raw = sp.check_output("multiqc --version", shell=True).decode("utf-8").replace("\n", "")
-    version_multiqc = search("[0-9]{1,2}\\.[0-9]{1,2}", version_multiqc_raw).group(0)
+    version_multiqc_raw = (
+        sp.check_output("multiqc --version", shell=True)
+        .decode("utf-8")
+        .replace("\n", "")
+    )
+    version_multiqc = search(
+        "[0-9]{1,2}\\.[0-9]{1,2}",
+        version_multiqc_raw,
+    ).group(0)
 
-    if not(run_cram):
+    if not run_cram:
         program_version_dict = {
             "Program": ["FastQC", "STAR", "HTSeq", "MultiQC"],
-            "Version": [version_fastqc, version_star, version_htseq, version_multiqc]
+            "Version": [version_fastqc, version_star, version_htseq, version_multiqc],
         }
-    elif run_cram :
-        version_samtools_raw = sp.check_output("samtools --version | grep samtools", shell=True).decode("utf-8").replace("\n", "")
-        version_samtools = search("[0-9]{1,2}\\.[0-9]{1,2}", version_samtools_raw).group(0)
+    else:
+        version_samtools_raw = sp.check_output(
+            "samtools --version | grep samtools",
+            shell=True,
+        ).decode("utf-8").replace("\n", "")
+        version_samtools = search(
+            "[0-9]{1,2}\\.[0-9]{1,2}",
+            version_samtools_raw,
+        ).group(0)
 
         program_version_dict = {
             "Program": ["FastQC", "STAR", "HTSeq", "MultiQC", "Samtools"],
-            "Version": [version_fastqc, version_star, version_htseq, version_multiqc, version_samtools]
+            "Version": [
+                version_fastqc,
+                version_star,
+                version_htseq,
+                version_multiqc,
+                version_samtools,
+            ],
         }
-    
+
     program_version_df = pd.DataFrame(program_version_dict)
     program_version_df.to_csv("version_information.csv", index=False)
 
 
-## Run the functions
+# Run the functions
 # run_fastqc(fastq_string, threads)
 # run_star(df, genome_dir, threads)
 run_htseq(df, strand, gtf)
